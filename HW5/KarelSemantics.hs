86c2c4abeb67e9e0d56db064206440d6d87dc9bd
Zachary Horine <horine.zachary@gmail.com>
2019-02-26 19:24:15 -0800

    formatting changes

--- HW5/KarelSemantics.hs
+++ HW5/KarelSemantics.hs
@@ -21,23 +21,23 @@ test (Empty) _ c     = isEmpty c
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown   _ _ r     = Done r
-stmt Move _ w r           = if test (Clear Front) w r
-                              then OK w (setPos (relativePos Front r))
                               else Error ("The space you are trying to move to is a wall." ++ show (relativePos Front r))
-stmt PickBeeper _ w r     = let p = getPos r
-                            in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
 stmt PutBeeper _ w r      = if test (Empty) w r
                               then Error ("Beeper Bag is empty!")
-                              else OK (incBeeper (getPos r)) (decBag r)
-stmt Turn    (d) w r      = OK w (setFacing (cardTurn d (getFacing r)) r)
-stmt Call    (m) w r      =
 stmt Iterate (i,s) w r    =
 stmt If      (t,s,st) w r =
 stmt While   (t,s) w r    =
 stmt Block   (s:st) w r   =
+stmt Shutdown _ _ r     = Done r
+stmt Move d w r             = if test (Clear Front) w r
+                                 then OK w (setPos (relativePos Front r))
+stmt PickBeeper _ w r       = let p = getPos r
+                              in if hasBeeper p w
+                                 then OK (decBeeper p w) (incBag r)
+                                 else Error ("No beeper to pick at: " ++ show p)
+                                 else OK (incBeeper (getPos r) w) (decBag r)
+stmt (Turn d) d w r         = OK w (setFacing (cardTurn d (getFacing r)) r)
+stmt (Call m) d w r         =
 stmt _ _ _ _ = undefined
 
 -- | Run a Karel program.
