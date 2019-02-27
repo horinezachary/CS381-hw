6248cd9cd4810aaf2bcf811ab6ee1c39ad2a1bcb
Zachary Horine <horine.zachary@gmail.com>
2019-02-26 19:24:47 -0800

    fix output messages and ad iterate if and while

--- HW5/KarelSemantics.hs
+++ HW5/KarelSemantics.hs
@@ -21,23 +21,33 @@ test (Empty) _ c     = isEmpty c
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-                              else Error ("The space you are trying to move to is a wall." ++ show (relativePos Front r))
-stmt PutBeeper _ w r      = if test (Empty) w r
-                              then Error ("Beeper Bag is empty!")
-stmt Iterate (i,s) w r    =
-stmt If      (t,s,st) w r =
-stmt While   (t,s) w r    =
-stmt Block   (s:st) w r   =
 stmt Shutdown _ _ r     = Done r
 stmt Move d w r             = if test (Clear Front) w r
                                  then OK w (setPos (relativePos Front r))
+                                 else Error ("Blocked at:" ++ show (relativePos Front r))
 stmt PickBeeper _ w r       = let p = getPos r
                               in if hasBeeper p w
                                  then OK (decBeeper p w) (incBag r)
                                  else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper d w r        = if isEmpty r
+                                 then Error ("No beeper to put.")
                                  else OK (incBeeper (getPos r) w) (decBag r)
 stmt (Turn d) d w r         = OK w (setFacing (cardTurn d (getFacing r)) r)
 stmt (Call m) d w r         =
+stmt (Iterate 0 s) d w r    = OK w r
+stmt (Iterate i s) d w r    = case stmt s d w r of
+                                 OK retW retR -> stmt (Iterate (i-1) s) d retW retR
+                                 Error e -> Error e
+stmt (If q s1 s2) d w r     = if test q w r
+                                 then stmt s1 d w r
+                                 else stmt s2 d w r
+stmt (While q s) d w r      = if test q w r
+                                 then OK w r
+                                 else case stmt s d w r of
+                                    OK retW retR -> stmt (while q s) d retW retR
+                                    Error e -> Error e
+stmt (Block []) d w r       = OK w r
+stmt (Block (s:st)) d w r   =
 stmt _ _ _ _ = undefined
 
 -- | Run a Karel program.
