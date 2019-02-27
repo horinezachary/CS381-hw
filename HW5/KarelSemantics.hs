fa59c4cb727b75d4dfbd771ca6b9da6d66cac5cb
thenellg <thenellg@oregonstate.edu>
2019-02-26 21:38:44 -0800

    Actually fixed the bugs

--- HW5/KarelSemantics.hs
+++ HW5/KarelSemantics.hs
@@ -19,45 +19,49 @@ test (Clear e) b c  = isClear (relativePos e c) b
 test (Beeper) b c    = hasBeeper (getPos c) b
 test (Empty) _ c     = isEmpty c
 
--- | Valuation function for Stmt.
-stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown _ _ r     = Done r
-stmt Move d w r             = if test (Clear Front) w r
-                                 then OK w (setPos (relativePos Front r) r)
-                                 else Error ("Blocked at: " ++ show (relativePos Front r))
-stmt PickBeeper _ w r       = let p = getPos r
-                              in if hasBeeper p w
-                                 then OK (decBeeper p w) (incBag r)
-                                 else Error ("No beeper to pick at: " ++ show p)
-stmt PutBeeper d w r        = if isEmpty r
-                                 then Error ("No beeper to put.")
-                                 else OK (incBeeper (getPos r) w) (decBag r)
-stmt (Turn dir) d w r       = OK w (setFacing (cardTurn dir (getFacing r)) r)
-stmt (Call m) d w r         = case lookup m d of
-                                 (Just a) -> stmt a d w r
-                                 _ -> Error ("Undefined macro: " ++ m)
-stmt (Iterate 0 s) d w r    = OK w r
-stmt (Iterate i s) d w r    = if i > 0
-                                then case stmt s d w r of
-                                 OK retW retR -> stmt (Iterate (i-1) s) d retW retR
-                                 Done retR -> Done retR
-                                 Error e -> Error e
-stmt (If q s1 s2) d w r     = if test q w r
-                                 then stmt s1 d w r
-                                 else stmt s2 d w r
-stmt (While q s) d w r      = if test q w r
-                                 then case stmt s d w r of
-                                    OK retW retR -> stmt (While q s) d retW retR
-                                    Done retR -> Done retR
-                                    Error e -> Error e
-                                 else OK w r
-stmt (Block []) _ w r       = OK w r
-stmt (Block (s:st)) d w r   = case stmt s d w r of
-                                 OK retW retR -> stmt (Block st) d w r
-                                 Done retR -> Done retR
-                                 Error e -> Error e
+-- | Valuation function for Stmt.--
+
 -- stmt _ _ _ _ = undefined
 
+
+stmt :: Stmt -> Defs -> World -> Robot -> Result
+stmt Shutdown _ _ r       = Done r
+stmt Move _ w r           = let p = relativePos Front r
+                            in if isClear p w
+                                  then OK w (setPos p r)
+                                  else Error ("Blocked at: " ++ show p)
+stmt PickBeeper _ w r     = let p = getPos r
+                            in if hasBeeper p w
+                                  then OK (decBeeper p w) (incBag r)
+                                  else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper _ w r      = let p = getPos r
+                            in if isEmpty r
+                                  then Error ("No beeper to put.")
+                                  else OK (incBeeper p w) (decBag r)
+stmt (Turn d) _ w r       = OK w (setFacing (cardTurn d (getFacing r)) r)
+stmt (Call m) d w r       = case lookup m d of
+                                 (Just a) -> stmt a d w r
+                                 _        -> Error ("Undefined macro: " ++ m)
+stmt (Iterate i s) d w r  = if i > 0
+                               then case stmt s d w r of
+                                         (OK retW retR) -> stmt (Iterate (i-1) s) d retW retR
+                                         (Done retR)  -> Done retR
+                                         (Error e)  -> Error e
+                               else OK w r
+stmt (If q stmt1 stmt2) d w r   = if (test q w r)
+                                then stmt stmt1 d w r
+                                else stmt stmt2 d w r
+stmt (While q s) d w r    = if (test q w r)
+                               then case stmt s d w r of
+                                    (OK retW retR) -> stmt (While q s) d retW retR
+                                    (Done retR)  -> Done retR
+                                    (Error e)  -> Error e
+                               else OK w r
+stmt (Block []) _ w r     = OK w r
+stmt (Block (s:ss)) d w r = case stmt s d w r of
+                                 (OK retW retR) -> stmt (Block ss) d retW retR
+                                 (Done retR)  -> Done retR
+                                 (Error e)  -> Error e
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
