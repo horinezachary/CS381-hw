9fa4013d86f9dfab1140e741353af3eda6ad3e97
Zachary Horine <horine.zachary@gmail.com>
2019-02-26 18:16:24 -0800

    add stubs for statement types and added Move, PutBeeper and Turn

--- HW5/KarelSemantics.hs
+++ HW5/KarelSemantics.hs
@@ -21,13 +21,21 @@ test (Empty) _ c     = isEmpty c
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown   _ _ r = Done r
-stmt PickBeeper _ w r = let p = getPos r
-                        in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
+stmt Shutdown   _ _ r     = Done r
+stmt Move _ w r           = OK w (setPos (relativePos Front r))
+stmt PickBeeper _ w r     = let p = getPos r
+                                 in if hasBeeper p w
+                                    then OK (decBeeper p w) (incBag r)
+                                    else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper _ w r      = OK (incBeeper (getPos r)) (decBag r)
+stmt Turn    (d) w r      = OK w (setFacing (cardTurn d (getFacing r)) r)
+stmt Call    (m) w r      =
+stmt Iterate (i,s) w r    =
+stmt If      (t,s,st) w r =
+stmt While   (t,s) w r    =
+stmt Block   (s:st) w r   =
 stmt _ _ _ _ = undefined
-    
+
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
